111c817462908f42d6e1bc21a80775f498729478
Student <example@example.com>
2020-02-28 11:05:40 -0800

    adjust Bread method to increment Price directly based on quantity.

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -11,14 +11,30 @@ namespace Bakery.Models
         // Give class auto-implemented property of 'Quantity' //
         // 'get' 'set' preform same behavior as privatizing. //
         public int Quantity { get; set; }
+        public int Price { get; set; }
 
-        public Bread()
+        public Bread(int quantity)
         {
-            Quantity = 0;
+            Quantity = quantity;
+            Price = quantity * 2;
         }
         public static void BreadInfo()
         {
-            Console.WriteLine("Here is our current selection");
+            Console.WriteLine("Our current selection is only limited to one loaf, for the time being. Would you like to buy a loaf of bread?");
+            Console.WriteLine("[yes] [no]");
+            string reponseBread = Console.ReadLine();
+            reponseBread = reponseBread.ToLower();
+            if (reponseBread == "yes")
+            {
+
+            }
+
+        }
+        public static void BuyThisBread()
+        {
+            Console.WriteLine("How much of this loaf would you like to purchase?");
+            Console.WriteLine("Bread is $2/loaf.");
+            int Amount = 
         }
     }
 }
