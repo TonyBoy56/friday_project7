53287f912fb82ac04d1d6d0e27cff50f89379371
Student <example@example.com>
2020-02-28 10:04:30 -0800

    begin BreadInfo() method.

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -1,1 +1,24 @@
 using System;
+using System.Collections.Generic;
+using Bakery;
+using Bakery.Models;
+
+namespace Bakery.Models
+{
+    // Create Bread class inside of Bakery namespace //
+    public class Bread
+    {
+        // Give class auto-implemented property of 'Quantity' //
+        // 'get' 'set' preform same behavior as privatizing. //
+        public int Quantity { get; set; }
+
+        public Bread()
+        {
+            Quantity = 0;
+        }
+        public static void BreadInfo()
+        {
+            Console.WriteLine("Here is our current")
+        }
+    }
+}
