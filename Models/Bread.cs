8717f5f2879af723ec44144c1229c34b61e0a931
Student <example@example.com>
2020-02-28 11:46:05 -0800

    clean up any comments.

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -5,11 +5,8 @@ using Bakery.Models;
 
 namespace Bakery.Models
 {
-    // Create Bread class inside of Bakery namespace //
     public class Bread
     {
-        // Give class auto-implemented property of 'Quantity' //
-        // 'get' 'set' preform same behavior as privatizing. //
         public int Quantity { get; set; }
         public int Price { get; set; }
 
@@ -17,24 +14,6 @@ namespace Bakery.Models
         {
             Quantity = quantity;
             Price = quantity * 2;
-        }
-        public static void BreadInfo()
-        {
-            Console.WriteLine("Our current selection is only limited to one loaf, for the time being. Would you like to buy a loaf of bread?");
-            Console.WriteLine("[yes] [no]");
-            string reponseBread = Console.ReadLine();
-            reponseBread = reponseBread.ToLower();
-            if (reponseBread == "yes")
-            {
-
-            }
-
-        }
-        public static void BuyThisBread()
-        {
-            Console.WriteLine("How much of this loaf would you like to purchase?");
-            Console.WriteLine("Bread is $2/loaf.");
-            int Amount = 
         }
     }
 }
