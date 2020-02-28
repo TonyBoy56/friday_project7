90a85386de24f5b0513991651788d1240f5ffa17
Student <example@example.com>
2020-02-28 10:03:54 -0800

    create instance of Bread and Pastry class. Add Main() and welcome messages, prompt for user response.

--- Bakery.cs
+++ Bakery.cs
@@ -1,6 +1,23 @@
 using System;
 using System.Collections.Generic;
+using Bakery.Models;
 
-namespace Bakery.Models {
-
+namespace Bakery 
+{
+    public class Program
+    {
+        // Create objects of Bread and Pastry classes //
+        public static Bread typeA = new Bread();
+        public static Pastry typeB = new Pastry();
+        // Here will go initial text prompting the user for inputs //
+        public static void Main()
+        {
+            Console.WriteLine("Hello! Welcome to Tony's Bakery!");
+            Console.WriteLine("Here are our current options:");
+            Console.WriteLine("Buy: [bread] [pastry] View Menus: [menus]");
+            
+            string response = Console.ReadLine();
+            response = response.toLower();
+        }
+    }
 }
