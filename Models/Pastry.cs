eb7900a8519bb0aca31b4c1b7ef4cb2c301bf7d5
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-03-08 16:00:31 -0700

    begin functionality for sales in the pastry class.

--- Models/Pastry.cs
+++ Models/Pastry.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using Bakery;
 using Bakery.Models;
 
-namespace Bakery.Models
+namespace Bakery.Models 
 {
-    public class Pastry
-    {
-        public int Quantity { get; set; }
-        public int Price { get; set; }
+  public class Pastry 
+  {
+    public int Quantity { get; set; }
+    public int Price { get; set; }
 
-        public Pastry(int quantity)
-        {
-            Quantity = quantity;
-            Price = quantity * 2;
-        }
+    public Pastry (int quantity) 
+    {
+      Quantity = quantity;
+      Price = quantity * 2;
+
+      if (quantity % 3 == 0)
+      {
+        Price -= 1;
+        Console.WriteLine("There's a buy 3 for $5 deal today! Your quantity is " + quantity + " for the price of " + Price + ".");
+      }
     }
+  }
 }
